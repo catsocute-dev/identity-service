cacc8467a220ae034205f14d3c69021a903add36
catsocute <nguyenlenhat205@gmail.com>
2025-08-10 14:28:03 +0700

    commit configuration

---
+++ src/main/java/com/catsocute/identity_service/configuration/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.catsocute.identity_service.configuration;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.experimental.NonFinal;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] PUBLIC_POST_ENDPOINT = {
+        "/users",
+        "auth/log-in",
+        "auth/introspect"
+    };
+
+    @NonFinal
+    @Value("${security.jwt.signer-key}")
+    private String SIGNER_KEY;
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> 
+            request.requestMatchers(HttpMethod.POST, PUBLIC_POST_ENDPOINT).permitAll()
+            .anyRequest().authenticated());
+
+        httpSecurity.oauth2ResourceServer(oauth2 -> 
+            oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
+        //disable csrf
+        httpSecurity.csrf(httpSecurityCsrfconfigurer -> httpSecurityCsrfconfigurer.disable());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(SIGNER_KEY.getBytes(), "HS512");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec)
+            .macAlgorithm(MacAlgorithm.HS512)
+            .build();
+    }
+}
