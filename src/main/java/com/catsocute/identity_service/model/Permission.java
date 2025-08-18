32254d8eefdfe2deb3d5a0dc9b3a18a5f7612b23
catsocute <nguyenlenhat205@gmail.com>
2025-08-18 14:32:55 +0700

    create 2 table role & permission

---
+++ src/main/java/com/catsocute/identity_service/model/Permission.java
@@ -0,0 +1,22 @@
+package com.catsocute.identity_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@Entity
+public class Permission {
+    @Id
+    String name;
+    String description;
+}
